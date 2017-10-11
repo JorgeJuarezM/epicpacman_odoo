1a9a07ada655588e60d29549bc18710cba19c96c
Jorge Juarez <contacto@jorgejuarez.net>
2017-10-11 01:48:27 -0400

    Avance

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -6,11 +6,28 @@ from odoo.addons.web.controllers.main import WebClient, Binary, Home
 import logging
 import random
 import json
+import base64
+import io
+import imghdr
 
 _logger = logging.getLogger(__name__)
 
 
 class EpicWeb(Home):
+    
+    @http.route('/images/<int:product_id>', type='http', auth="public")
+    def images(self, product_id):
+        
+        product = request.env['product.template'].browse([product_id])
+        imgname = 'logo'
+        imgext = '.png'
+                   
+        image_base64 = base64.b64decode(product.image)
+        image_data = io.BytesIO(image_base64)
+        imgext = '.' + (imghdr.what(None, h=image_base64) or 'png')
+        response = http.send_file(image_data, filename=imgname + imgext)
+        return response
+    
     @http.route('/', type='http', auth="public")
     def Home(self, **kw):
         
@@ -36,8 +53,20 @@ class EpicWeb(Home):
         search_product = list(products)
         random.shuffle(search_product)
         
+        products = []
+        
+        for p in search_product:
+            products.append({
+                'id': p.id,
+                'price': p.list_price,
+                'description': p.description_picking,
+                'description_sale': p.description_sale,
+                'name': p.name,
+                'class_type': p.class_type
+            })
+        
         return request.render('epic.homepage', {
-            'products': search_product
+            'products': products
         })
         
 
